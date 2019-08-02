3c3c8be4dc6dc71a59d63c5648a8d996b39407cc
Luis <lmrcarneiro@gmail.com>
2019-08-02 17:09:52 +0100

    reorganized file structure and connected front and back end

--- src/image_gallery/urls.py
+++ src/image_gallery/urls.py
@@ -16,18 +16,37 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+#teste
 from pages.views import home_view
 from images.views import images_view
 from contact.views import contact_create_view
 
+#paginas utilizadas
+from pages.views import index_view
+from pages.views import about_view
+from pages.views import code_view
+from pages.views import solutions_view
+from pages.views import faq_view
+
+
 #static
 from django.conf import settings
 from django.conf.urls.static import static
-
+#from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-	path('', home_view, name='home'),
+	# TESTE
+    # path('', home_view, name='home'), 
 	path('images/', images_view, name='images'),
     path('contact/', contact_create_view, name='contact_form'),
+    
+    # a ser utilizado
+    path('', index_view, name='home'),
+    path('about/', about_view, name='about'),
+    path('code/', code_view, name='code'),
+    path('solutions/', solutions_view, name='solutions'),
+    path('FAQ/', faq_view, name='faq'),
     path('admin/', admin.site.urls),
 ] +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- src/pages/views.py
+++ src/pages/views.py
@@ -1,9 +1,33 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#teste
 def home_view(request, *args, **kwargs):
 	context = {}
 	return render(request, "home.html", context)
 
 def contact_view(request, *args, **kwargs):
 	return render(request, "contact_form.html", {})
+
+
+#a ser utilizadas
+def index_view(request, *args, **kwargs):
+	context = {}
+	return render(request, "index.html", context)
+
+def about_view(request, *args, **kwargs):
+	context = {}
+	return render(request, "about.html", context)
+
+def code_view(request, *args, **kwargs):
+	context = {}
+	return render(request, "code.html", context)
+
+def solutions_view(request, *args, **kwargs):
+	context = {}
+	return render(request, "solutions.html", context)
+
+def faq_view(request, *args, **kwargs):
+	context = {}
+	return render(request, "FAQ.html", context)
